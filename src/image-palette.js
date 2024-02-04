a3315edffdac49e4acda43a9af13cc7c8316a63c
Marco Vinciguerra <88108002+VinciGit00@users.noreply.github.com>
2024-02-04 21:54:54 +0100

    Update image-palette.js

--- src/image-palette.js
+++ src/image-palette.js
@@ -6,7 +6,7 @@ var ImagePalette = function () { };
 ImagePalette.prototype.getColorPalette = (image, quality, returnType) => {
     utils.validation(quality, 3, returnType)
     const pixelArray = utils.getPixelArray(image, 3, quality);
-    return returnType == "rgb" ? findMostFrequentCombination(pixelArray): utils.convertRgbToHex(findMostFrequentCombination(pixelArray))
+    return returnType == "rgb" ? findMostFrequentCombination(pixelArray, 1): utils.convertRgbToHex(findMostFrequentCombination(pixelArray))
 }
 
 export default ImagePalette;
