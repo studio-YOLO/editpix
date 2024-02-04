e3c0aa75651bc8262edc72ba4d265d823bd405ed
Marco Vinciguerra <88108002+VinciGit00@users.noreply.github.com>
2024-02-04 21:54:27 +0100

    Update color-selection.js

--- src/color-selection.js
+++ src/color-selection.js
@@ -1,21 +1,26 @@
-function findMostFrequentCombination(array) {
-    var max = 0; 
-    var best = "";
-    
-    var dict = {};
-    
-    for (let i = 0; i < array.length - 2; i++) {
-        var key = array[i].toString() ;
-      
-        if (key in dict) dict[key]++;
-        else dict[key] = 1;
+function findMostFrequentCombination(matrix, number_feature_result) {
+  var dict = {};
 
-        if (dict[key] > max) {
-            max = dict[key]; 
-            best = key;
-        }
-    } 
-    return best.split(",").map(Number);
+  // Count the frequency of each RGB combination
+  for (let i = 0; i < matrix.length; i++) {
+    var key = matrix[i].toString();
+
+    if (key in dict) {
+      dict[key]++;
+    } else {
+      dict[key] = 1;
+    }
+  }
+
+  // Sort the combinations based on frequency in ascending order
+  var sortedCombinations = Object.keys(dict).sort(function (a, b) {
+    return dict[a] - dict[b];
+  });
+
+  // Get the top N combinations
+  var result = sortedCombinations.slice(0, number_feature_result);
+
+  return result;
 }
 
 export default findMostFrequentCombination;
