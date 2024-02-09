d11e97a247767650be8b29aafc6f6060825e26b9
DPende <pendesini.daniele@gmail.com>
2024-02-09 10:53:30 +0100

    feat: added function thet return an array of pixels for rendering with html canvas

--- src/image-palette.js
+++ src/image-palette.js
@@ -1,12 +1,21 @@
 import utils from "./utils.js"
 import findMostFrequentCombination from "./color-selection.js";
+import quantize from "./quantization.js";
+import decript from "./decript.js"
 
 var ImagePalette = function () { };
 
-ImagePalette.prototype.getColorPalette = (image, quality, returnType) => {
-    utils.validation(quality, 3, returnType)
-    const pixelArray = utils.getPixelArray(image, 3, quality);
-    return returnType == "rgb" ? findMostFrequentCombination(pixelArray, 1): utils.convertRgbToHex(findMostFrequentCombination(pixelArray))
+ImagePalette.prototype.getColorPalette = (image, colorNumber, quality, returnType) => {
+    utils.validation(quality, colorNumber, returnType)
+    const pixelArray = utils.getPixelArray(image, quality);
+    return returnType == "rgb" ? findMostFrequentCombination(pixelArray, colorNumber): utils.convertRgbToHex(findMostFrequentCombination(pixelArray, colorNumber))
 }
+
+ImagePalette.prototype.getPixelArray = (image, quality, bitNumber) => {
+    const pixelArray = utils.getPixelArray(image, quality);
+    return decript(quantize(pixelArray, bitNumber))
+}
+
+
 
 export default ImagePalette;
