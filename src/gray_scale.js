6358f9e05da11403bd8b17bfccfb3477cc84eb2c
VinciGit00 <mvincig11@gmail.com>
2024-02-15 11:43:02 +0100

    refactoring of functions

--- src/gray_scale.js
+++ src/gray_scale.js
@@ -1,23 +1,19 @@
 /**
- *  Functiom that given array in the format [[[R,G,B],..., [R,G,B]] it converts it gray scale in format [g1, ..., gn]
- *  @param {number[]} image_to_convert: image that has to be encrypt in the format [[[R,G,B],..., [R,G,B]]
- *  @returns {number[][]} encrypted_image: gray scale  image in the format [g1, ..., gn]
+ *  Functiom that given array in the format [[R,G,B],..., [R,G,B]] it converts it gray scale in format [[g1,g1,g1], ..., [gn,gn,gn]]
+ *  @param {number[]} image_to_convert: image that has to be encrypt in the format [[R,G,B],..., [R,G,B]]
+ *  @returns {number[][]} encrypted_image: gray scale  image in the format [[g1,g1,g1], ..., [gn,gn,gn]]
  */
 function convert_to_gray_scale(image_to_convert) {
   var gray_scaled_image = [];
-  for (let i = 0; i < image_to_convert.length / 4; i++) {
-    if (i == 0)
-      gray_scaled_image.push([
-        0.299 * image_to_convert[0] +
-          0.587 * image_to_convert[1] +
-          0.114 * image_to_convert[2],
-      ]);
-    else
-      gray_scaled_image.push([
-        0.299 * image_to_convert[4 * i] +
-          0.587 * image_to_convert[4 * i + 1] +
-          0.114 * image_to_convert[4 * i + 2],
-      ]);
+  for (let i = 0; i < image_to_convert.length; i++) {
+    var elem_to_add = [
+       Math.round( 0.299 * image_to_convert[i][0] +
+        0.587 * image_to_convert[i][1] +
+        0.114 * image_to_convert[i][2]),
+      ];
+
+      gray_scaled_image.push([elem_to_add, elem_to_add, elem_to_add]);
+    
   }
   return gray_scaled_image;
 }
--- src/gray_to_rgb.js
+++ src/gray_to_rgb.js
@@ -1,23 +1,16 @@
 /**
- *  Functiom that given array in the format [[[R,G,B],..., [R,G,B]] it converts it gray scale in format [g1, ..., gn]
- *  @param {number[]} image_to_convert: image that has to be encrypt in the format [[[R,G,B],..., [R,G,B]]
- *  @returns {number[][]} encrypted_image: gray scale  image in the format [g1, ..., gn]
+ *  Function that given array in the format [[g1,g1,g1],..., [gn,gn,gn]] it converts it gray scale in format [[R,G,B,255],..., [R,G,B,255]]
+ *  @param {number[]} image_to_convert: image that has to be encrypt in the format [[g1,g1,g1],..., [gn,gn,gn]]
+ *  @returns {number[][]} encrypted_image: gray scale  image in the format [[R,G,B,255],..., [R,G,B,255]]
  */
 function convert_to_rgb_from_gray(image_to_convert) {
   var rgb_image = [];
-  for (let i = 0; i < image_to_convert.length / 4; i++) {
-    if (i == 0)
+  for (let i = 0; i < image_to_convert.length; i++) {
+   
       rgb_image.push([
-        image_to_convert[0] / 0.299,
-        image_to_convert[1] / 0.587,
-        image_to_convert[2] / 0.114,
-        255,
-      ]);
-    else
-      rgb_image.push([
-        image_to_convert[4 * i] / 0.299,
-        image_to_convert[4 * i + 1] / 0.587,
-        image_to_convert[4 * i + 2] / 0.114,
+        Math.round(image_to_convert[i][0] / 0.299),
+        Math.round(image_to_convert[i][0] / 0.587),
+        Math.round(image_to_convert[i][0] / 0.114),
         255,
       ]);
   }
