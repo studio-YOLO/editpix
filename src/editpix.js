68df4a06cf571a1cc83c6b83598a32f7b26f170c
DPende <pendesini.daniele@gmail.com>
2024-02-15 22:23:16 +0100

    add: added new functions

--- src/editpix.js
+++ src/editpix.js
@@ -1,21 +1,52 @@
 import utils from "./utils.js"
-import findMostFrequentCombination from "./color-selection.js";
-import quantize from "./quantization.js";
-import decript from "./decript.js"
+import convert_to_gray_scale from "./core/gray_scale.js";
+import convert_to_black_and_white from "./core/colors_to_black_and_white.js";
+import kMeans from "./core/kmean.js";
 
-var ImagePalette = function () { };
+var EditPix = function () { };
 
-ImagePalette.prototype.getColorPalette = (image, colorNumber, quality, returnType) => {
-    utils.validation(quality, colorNumber, returnType)
+EditPix.prototype.getColorPalette = (image, colorNumber = 5, quality = 1) => {
+    utils.validate(quality, colorNumber, returnType);
     const pixelArray = utils.getPixelArray(image, quality);
-    return returnType == "rgb" ? findMostFrequentCombination(pixelArray, colorNumber): utils.convertRgbToHex(findMostFrequentCombination(pixelArray, colorNumber))
+    return kMeans(pixelArray, colorNumber);
 }
 
-ImagePalette.prototype.getPixelArray = (image, quality, bitNumber) => {
+EditPix.prototype.getDominantColor = (image, quality = 1) => {
+    utils.validate(quality, colorNumber, returnType);
     const pixelArray = utils.getPixelArray(image, quality);
-    return decript(quantize(pixelArray, bitNumber))
+    return kMeans(pixelArray, 1);
+}
+
+EditPix.prototype.getImageFromUrl = (url) => {
+    const image = new Image();
+    image.url = url;
+    return image;
+}
+
+EditPix.prototype.toGrayScale = (image) => {
+    //TODO
 }
 
 
+EditPix.prototype.fromGrayScaleToRgb = (image) => {
+    //TODO
+}
 
-export default ImagePalette;
+EditPix.prototype.toBackWhite = (image) => {
+    //TODO
+}
+
+EditPix.prototype.getHigherContrast = (color) => {
+    //TODO
+}
+
+EditPix.prototype.convertToHex = (colors) => {
+    return utils.rgbToHex(colors);
+}
+
+EditPix.prototype.convertToRgb = (colors) => {
+    return utils.hexToRgb(colors);
+}
+
+
+export default EditPix;
