f44a03139a3f13c109464a84295951ae6b060403
DPende <pendesini.daniele@gmail.com>
2024-02-18 21:50:18 +0100

    refactoring: changed variables names

--- src/core/gray_to_rgb.js
+++ src/core/gray_to_rgb.js
@@ -1,20 +1,19 @@
 /**
  *  Function that given array in the format [g1,g1,g1,alfa1,...,gn,gn,gn,alfan] it converts it gray scale in format [R,G,B,alfa,...,R,G,B,alfa]
- *  @param {number[]} image_to_convert: image that has to be encrypt in the format [g1,g1,g1,alfa1,...,gn,gn,gn,alfan]
+ *  @param {number[]} pixelArray: image that has to be encrypt in the format [g1,g1,g1,alfa1,...,gn,gn,gn,alfan]
  *  @returns {number[][]} encrypted_image: gray scale  image in the format [R,G,B,alfa,...,R,G,B,alfa]
  */
-function convert_to_rgb_from_gray(image_to_convert) {
-  var rgb_image = [];
-  for (let i = 0; i < image_to_convert.length/4; i++) {
-   
-      rgb_image.push(
-        Math.round(image_to_convert[4*i] / 0.299),
-        Math.round(image_to_convert[4*i] / 0.587),
-        Math.round(image_to_cenvert[4*i] / 0.114),
-        image_to_convert[4*i+3]
-      );
+function convertFronGrayToRgb(pixelArray) {
+  var rgbPixelArray = [];
+  for (let i = 0; i < pixelArray.length / 4; i++) {
+    rgbPixelArray.push(
+      Math.round(pixelArray[4 * i] / 0.299),
+      Math.round(pixelArray[4 * i] / 0.587),
+      Math.round(pixelArray[4 * i] / 0.114),
+      pixelArray[4 * i + 3]
+    );
   }
-  return rgb_image;
+  return rgbPixelArray;
 }
 
-export default convert_to_rgb_from_gray;
+export default convertFronGrayToRgb;
--- src/core/higher_contrast.js
+++ src/core/higher_contrast.js
@@ -1,10 +1,10 @@
 /**
- *  Functiom that given a color in the format [R,G,B] it converts it gray scale in format [R,G,B]
- *  @param {number[]} color_to_analyze:color that has to be calculated the one with the higher contrast in the format [R,G,B]
- *  @returns {number[]} color with the higher contrast of color_to_analyze in the format [R,G,B]
+ *  Function that given a color in the format [R,G,B] it converts it gray scale in format [R,G,B]
+ *  @param {number[]} color:color that has to be calculated the one with the higher contrast in the format [R,G,B]
+ *  @returns {number[]} color with the higher contrast of color in the format [R,G,B]
  */
-function higher_color_contrast(color_to_analyze) {
-  return [255-color_to_analyze[0],255-color_to_analyze[1],255-color_to_analyze[2]];
+function higherColorContrast(color) {
+  return [255-color[0],255-color[1],255-color[2]];
 }
 
-export default higher_color_contrast;
+export default higherColorContrast;
