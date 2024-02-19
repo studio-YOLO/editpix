f1ed5861b53f6c48eaee2deedfddea7bdb534a9a
DPende <pendesini.daniele@gmail.com>
2024-02-19 21:59:59 +0100

    add: added demo script

---
+++ demo/gray_scale/color_palette_demo.js
@@ -0,0 +1,45 @@
+import EditPix from "../../src/editpix.js";
+
+const editpix = new EditPix();
+
+const url = "../images/img1.jpg";
+
+var image = new Image();
+image.src = url;
+
+var container = document.createElement("div");
+container.classList.add("container")
+
+
+
+//waiting image load
+image.addEventListener("load", () => {
+    //calculate color palette
+    const colorPaletteRgb = editpix.getColorPalette(image, 10, 1);
+
+    //convert color palette in HEX format
+    const colorPaletteHex = editpix.convertToHex(colorPaletteRgb);
+
+    container.appendChild(image);
+
+    //print output
+
+    var colorContainer = document.createElement("div")
+    colorContainer.classList.add("color_container")
+    
+    colorPaletteHex.forEach(color => {
+
+        var colorDiv = document.createElement("div")
+        colorDiv.classList.add("color")
+        colorDiv.style.backgroundColor = color;
+        
+        var text = document.createTextNode(color)
+
+        colorDiv.appendChild(text)
+
+        colorContainer.appendChild(colorDiv)
+
+    });
+    container.appendChild(colorContainer)
+    document.body.appendChild(container)
+});
--- demo/gray_scale/script.js
+++ demo/gray_scale/script.js
@@ -1,59 +1,41 @@
-import convert_to_gray_scale from "../../src/convert_to_gray_scale.js";
+import EditPix from '../../src/editpix.js';
 
-// Funzione per caricare e visualizzare le immagini in scala di grigi
-function loadGrayScaleImages() {
-  // Array contenente i percorsi delle immagini
-  const imagePaths = [
-    "images/img1.jpg",
-    "images/img2.jpg",
-    "images/img3.jpeg",
-    "images/img4.webp",
-  ];
+const editpix = new EditPix();
 
-  // Loop attraverso le immagini e carica le versioni in scala di grigi
-  imagePaths.forEach((path, index) => {
-    // Creazione di un nuovo elemento immagine
-    const img = document.createElement("img");
-    img.src = path;
+const imagesUrl = [
+  '../images/img3.jpeg'
+];
 
-    // Quando l'immagine è caricata, viene eseguita la conversione in scala di grigi
-    img.onload = () => {
-      // Creazione di un canvas per estrarre i dati dell'immagine
-      const canvas = document.createElement("canvas");
-      const ctx = canvas.getContext("2d");
-      canvas.width = img.width;
-      canvas.height = img.height;
-      ctx.drawImage(img, 0, 0, img.width, img.height);
+// load images
+const originalImages = [];
+imagesUrl.forEach(url => {
+  const tmpImage = new Image();
+  tmpImage.src = url;
+  originalImages.push(tmpImage);
+});
 
-      // Estrazione dei dati dell'immagine in formato RGBA
-      const imageData = ctx.getImageData(0, 0, img.width, img.height).data;
-      const rgbArray = [];
-      for (let i = 0; i < imageData.length; i += 4) {
-        rgbArray.push([imageData[i], imageData[i + 1], imageData[i + 2]]);
-      }
-
-      // Conversione in scala di grigi utilizzando la funzione
-      const grayScaleArray = convert_to_gray_scale(rgbArray);
-
-      // Creazione di un nuovo canvas per visualizzare l'immagine in scala di grigi
-      const grayCanvas = document.createElement("canvas");
-      const grayCtx = grayCanvas.getContext("2d");
-      grayCanvas.width = img.width;
-      grayCanvas.height = img.height;
-
-      // Disegno dell'immagine in scala di grigi sul canvas
-      grayScaleArray.forEach((grayValue, i) => {
-        const x = i % img.width;
-        const y = Math.floor(i / img.width);
-        grayCtx.fillStyle = `rgb(${grayValue},${grayValue},${grayValue})`;
-        grayCtx.fillRect(x, y, 1, 1);
-      });
-
-      // Aggiunta del canvas alla pagina HTML
-      document.getElementById(`color${index + 1}`).appendChild(grayCanvas);
-    };
+// convert to gray scale
+originalImages.forEach(image => {
+  image.addEventListener('load', () => {
+    const grayScaledImage = editpix.toGrayScale(image);
+    const blackWhiteImage = editpix.toBackWhite(image);
+    const resizedImageQuality = editpix.resizeByQuality(image, 5);
+    const resizedImageWidth = editpix.resizeByWidth(image, 100);
+    const resizeImageHeight = editpix.resizeByHeight(image, 100);
+    console.log(editpix.getColorPalette(image, 10, 1));
+    grayScaledImage.addEventListener('load', () => {
+      const froGSToRgb = editpix.fromGrayScaleToRgb(grayScaledImage);
+      const container = document.createElement('div');
+      container.style.display = 'flex';
+      container.style.flexDirection = 'row';
+      container.appendChild(image);
+      container.appendChild(grayScaledImage);
+      container.appendChild(blackWhiteImage);
+      container.appendChild(froGSToRgb);
+      container.appendChild(resizedImageQuality);
+      container.appendChild(resizedImageWidth);
+      container.appendChild(resizeImageHeight);
+      document.body.appendChild(container);
+    });
   });
-}
-
-// Chiamata alla funzione per caricare e visualizzare le immagini in scala di grigi
-loadGrayScaleImages();
+});
