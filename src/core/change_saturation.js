dfe3e29488e4a2cb58628767c7521bd8dce04697
DPende <pendesini.daniele@gmail.com>
2024-03-02 17:00:50 +0100

    refactor: hsl-rgb conversion functions made independent

--- src/core/change_saturation.js
+++ src/core/change_saturation.js
@@ -1,98 +1,21 @@
+import utils from "../utils.js";
+
 /**
  * Function to change the saturation of an image
  * @param {number[]} pixelArray: Image pixel array in the format [R, G, B, alpha,..., R, G, B, alpha]
- * @param {number} saturationFactor: Saturation factor to adjust the saturation (-1 to 1)
+ * @param {number} factor: factor to adjust the saturation (-100 to 100)
  * @returns {number[]} adjustedPixelArray: Adjusted image pixel array in the format [R, G, B, alpha,..., R, G, B, alpha]
  */
-function changeSaturation(pixelArray, saturationFactor) {
+function changeSaturation(pixelArray, factor) {
     const adjustedPixelArray = [];
-    for (let i = 0; i < pixelArray.length / 4; i++) {
-        const r = pixelArray[4 * i];
-        const g = pixelArray[4 * i + 1];
-        const b = pixelArray[4 * i + 2];
+    for (let i = 0; i < pixelArray.length; i += 4) {
+        const hsl = utils.rgbToHsl(pixelArray[i], pixelArray[i + 1], pixelArray[i + 2]);
+        hsl[1] = Math.max(0, Math.min(100, hsl[1] += factor));
+        const rgb = utils.hslToRgb(hsl[0], hsl[1], hsl[2]);
 
-        // Convert RGB to HSL
-        const hsl = rgbToHsl(r, g, b);
-
-        // Adjust saturation
-        hsl[1] += saturationFactor;
-        hsl[1] = Math.max(0, Math.min(1, hsl[1])); // Ensure saturation is within [0, 1] range
-
-        // Convert HSL back to RGB
-        const rgb = hslToRgb(hsl[0], hsl[1], hsl[2]);
-
-        adjustedPixelArray.push(rgb[0], rgb[1], rgb[2], pixelArray[4 * i + 3]);
+        adjustedPixelArray.push(rgb[0], rgb[1], rgb[2], pixelArray[i + 3]);
     }
     return adjustedPixelArray;
-}
-
-/**
- * Function to convert RGB color to HSL
- * @param {number} r: Red value (0 to 255)
- * @param {number} g: Green value (0 to 255)
- * @param {number} b: Blue value (0 to 255)
- * @returns {number[]} HSL representation [Hue, Saturation, Lightness]
- */
-function rgbToHsl(r, g, b) {
-    r /= 255;
-    g /= 255;
-    b /= 255;
-
-    const max = Math.max(r, g, b);
-    const min = Math.min(r, g, b);
-    let h, s, l = (max + min) / 2;
-
-    if (max === min) {
-        h = s = 0; // achromatic
-    } else {
-        const d = max - min;
-        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
-        switch (max) {
-        case r:
-            h = (g - b) / d + (g < b ? 6 : 0);
-            break;
-        case g:
-            h = (b - r) / d + 2;
-            break;
-        case b:
-            h = (r - g) / d + 4;
-            break;
-        }
-        h /= 6;
-    }
-
-    return [h, s, l];
-}
-
-/**
- * Function to convert HSL color to RGB
- * @param {number} h: Hue value (0 to 1)
- * @param {number} s: Saturation value (0 to 1)
- * @param {number} l: Lightness value (0 to 1)
- * @returns {number[]} RGB representation [Red, Green, Blue]
- */
-function hslToRgb(h, s, l) {
-    let r, g, b;
-
-    if (s === 0) {
-        r = g = b = l; // achromatic
-    } else {
-        const hue2rgb = (p, q, t) => {
-            if (t < 0) t += 1;
-            if (t > 1) t -= 1;
-            if (t < 1 / 6) return p + (q - p) * 6 * t;
-            if (t < 1 / 2) return q;
-            if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
-            return p;
-        };
-        const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-        const p = 2 * l - q;
-        r = hue2rgb(p, q, h + 1 / 3);
-        g = hue2rgb(p, q, h);
-        b = hue2rgb(p, q, h - 1 / 3);
-    }
-
-    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
 }
 
 export default changeSaturation;
--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 function removeAlpha(pixelArray) {
     let result = [];
     for (let i = 0; i < pixelArray.length / 4; i++) {
-        if(pixelArray[i * 4 + 3] >=127){
+        if (pixelArray[i * 4 + 3] >= 127) {
             result.push([pixelArray[i * 4], pixelArray[i * 4 + 1], pixelArray[i * 4 + 2]]);
         }
     }
@@ -58,11 +58,61 @@ function deserializeArray(serializedArray) {
     return result;
 }
 
+/**
+ * Function to convert RGB color to HSL
+ * @param {number} r: Red value (0 to 255)
+ * @param {number} g: Green value (0 to 255)
+ * @param {number} b: Blue value (0 to 255)
+ * @returns {number[]} HSL representation [Hue, Saturation, Lightness]
+ */
+function rgbToHsl(r, g, b) {
+    if (r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255)
+        throw new Error("RGB format is invalid.")
+    r /= 255;
+    g /= 255;
+    b /= 255;
+    const l = Math.max(r, g, b);
+    const s = l - Math.min(r, g, b);
+    const h = s
+        ? l === r
+            ? (g - b) / s
+            : l === g
+                ? 2 + (b - r) / s
+                : 4 + (r - g) / s
+        : 0;
+    return [
+        Math.round(60 * h < 0 ? 60 * h + 360 : 60 * h),
+        Math.round(100 * (s ? (l <= 0.5 ? s / (2 * l - s) : s / (2 - (2 * l - s))) : 0)),
+        Math.round((100 * (2 * l - s)) / 2),
+    ];
+}
+
+/**
+ * Function to convert HSL color to RGB
+ * @param {number} h: Hue value (0 to 1)
+ * @param {number} s: Saturation value (0 to 1)
+ * @param {number} l: Lightness value (0 to 1)
+ * @returns {number[]} RGB representation [Red, Green, Blue]
+ */
+function hslToRgb(h, s, l) {
+    if (h < 0 || h > 360 || s < 0 || s > 100 || l < 0 || l > 100)
+        throw new Error("HSL format is invalid.")
+    s /= 100;
+    l /= 100;
+    const k = n => (n + h / 30) % 12;
+    const a = s * Math.min(l, 1 - l);
+    const f = n =>
+        l - a * Math.max(-1, Math.min(k(n) - 3, Math.min(9 - k(n), 1)));
+    return [255 * f(0), 255 * f(8), 255 * f(4)];
+}
+
 export default {
     rgbToHex,
     hexToRgb,
     validate,
     removeAlpha,
     removeAlphaSerialized,
-    deserializeArray
+    deserializeArray,
+    rgbToHsl,
+    hslToRgb
 };
