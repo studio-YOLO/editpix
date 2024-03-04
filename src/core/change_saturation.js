f7274fdae63e4f5ef0572116d9083ddd0db85eba
DPende <pendesini.daniele@gmail.com>
2024-03-04 23:29:31 +0100

    refactor: optimized function for image saturation/desaturation

--- src/core/change_saturation.js
+++ src/core/change_saturation.js
@@ -1,21 +1,22 @@
 import utils from "../utils.js";
 
 /**
- * Function to change the saturation of an image
- * @param {number[]} pixelArray: Image pixel array in the format [R, G, B, alpha,..., R, G, B, alpha]
- * @param {number} factor: factor to adjust the saturation (-100 to 100)
- * @returns {number[]} adjustedPixelArray: Adjusted image pixel array in the format [R, G, B, alpha,..., R, G, B, alpha]
+ * Function to change the saturation of an image.
+ * @param {number[]} pixelArray: Image pixel array in the format [R, G, B, alpha,..., R, G, B, alpha].
+ * @param {number} factor: factor to adjust the saturation (-100 to 100).
+ * @returns {number[]} pixel array of the saturated or desaturated image.
  */
 function changeSaturation(pixelArray, factor) {
-    const adjustedPixelArray = [];
     for (let i = 0; i < pixelArray.length; i += 4) {
         const hsl = utils.rgbToHsl(pixelArray[i], pixelArray[i + 1], pixelArray[i + 2]);
         hsl[1] = Math.max(0, Math.min(100, hsl[1] += factor));
         const rgb = utils.hslToRgb(hsl[0], hsl[1], hsl[2]);
 
-        adjustedPixelArray.push(rgb[0], rgb[1], rgb[2], pixelArray[i + 3]);
+        pixelArray[i] = rgb[0];
+        pixelArray[i + 1] = rgb[1];
+        pixelArray[i + 2] = rgb[2];
     }
-    return adjustedPixelArray;
+    return pixelArray;
 }
 
 export default changeSaturation;
