79ea8c1c6d5923994c67469d34fdb7c5e141dbdb
VinciGit00 <mvincig11@gmail.com>
2024-03-15 11:52:31 +0100

    add optimizated file for change shadow function

--- src/core/change_shadows.js
+++ src/core/change_shadows.js
@@ -1,13 +1,27 @@
+/**
+ * Modifies shadows in the pixel array by multiplying the RGB values of pixels
+ * with luminance less than 128 by the specified factor.
+ * 
+ * @param {Uint8ClampedArray} pixelArray - Array of pixels represented in RGBA format.
+ * @param {number} factor - Multiplication factor for pixels with luminance less than 128.
+ * @returns {Uint8ClampedArray} - The modified pixel array.
+ */
 function changeShadows(pixelArray, factor) {
-    for (let i = 0; i < pixelArray.length / 4; i++) {
-        const luminance = (pixelArray[4 * i] + pixelArray[4 * i + 1] + pixelArray[4 * i + 2]) / 3;
+    const len = pixelArray.length;
+    const factorVector = [factor, factor, factor, 1];
 
-        if (luminance < 128) {
-            pixelArray[4 * i] *= factor; 
-            pixelArray[4 * i + 1] *= factor; 
-            pixelArray[4 * i + 2] *= factor; 
+    for (let i = 0; i < len; i += 4) {
+        const luminance = (pixelArray[i] + pixelArray[i + 1] + pixelArray[i + 2]) / 3;
+        const isShadow = luminance < 128;
+
+        // If pixel is in shadow, multiply RGB values by factor
+        if (isShadow) {
+            for (let j = 0; j < 4; j++) {
+                pixelArray[i + j] *= factor;
+            }
         }
     }
+
     return pixelArray;
 }
 
