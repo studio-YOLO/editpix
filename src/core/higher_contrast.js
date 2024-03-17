0762a051a16414c0aed89aca0260818b5f7721f6
DPende <pendesini.daniele@gmail.com>
2024-03-17 16:22:53 +0100

    refactor: added demo script for higher contrast function

--- src/core/higher_contrast.js
+++ src/core/higher_contrast.js
@@ -1,21 +1,13 @@
 /**
  * Function that given a color in the format [R,G,B] converts it to grayscale in the format [R,G,B]
- * and then calculates the color with the highest contrast.
+ * and then calculates the color (black or white) with the highest contrast.
  * @param {number[]} color Color that has to be converted and compared in the format [R,G,B]
  * @returns {number[]} Color with the higher contrast of the input color in the format [R,G,B]
  */
 function higherColorContrast(color) {
-    // Convert the color to grayscale
-    const grayscale = [
-        Math.round(0.299 * color[0] + 0.587 * color[1] + 0.114 * color[2]),
-        Math.round(0.299 * color[0] + 0.587 * color[1] + 0.114 * color[2]),
-        Math.round(0.299 * color[0] + 0.587 * color[1] + 0.114 * color[2])
-    ];
-
-    // Calculate the color with the highest contrast
-    const contrastColor = grayscale.map(channel => channel > 128 ? 0 : 255);
-
-    return contrastColor;
+    const gray = Math.round(0.299 * color[0] + 0.587 * color[1] + 0.114 * color[2])
+    const contrastColor = gray >= 128 ? 0 : 255;
+    return [contrastColor, contrastColor, contrastColor];
 }
 
 export default higherColorContrast;
