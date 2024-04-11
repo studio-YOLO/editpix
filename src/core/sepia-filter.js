628c81a56c4268017d89638dace2b0f2eb21b009
DPende <pendesini.daniele@gmail.com>
2024-04-11 10:18:19 +0200

    refactor: removed clipping from sepia-filter

--- src/core/sepia-filter.js
+++ src/core/sepia-filter.js
@@ -4,19 +4,14 @@
  *  @returns {number[]} sepiaPixelArray: pixel array of a sepia-toned image
  */
 function toSepia(pixelArray) {
-
-    const clip = (value) => {
-        return value < 255 ? value : 255;
-    }
-
     for (let i = 0; i < pixelArray.length; i += 4) {
         let r = pixelArray[i];
         let g = pixelArray[i + 1];
         let b = pixelArray[i + 2];
 
-        pixelArray[i] = clip(r * 0.393 + g * 0.769 + b * 0.189);
-        pixelArray[i + 1] = clip(r * 0.349 + g * 0.686 + b * 0.168);
-        pixelArray[i + 2] = clip(r * 0.272 + g * 0.534 + b * 0.131);
+        pixelArray[i] = r * 0.393 + g * 0.769 + b * 0.189;
+        pixelArray[i + 1] = r * 0.349 + g * 0.686 + b * 0.168;
+        pixelArray[i + 2] = r * 0.272 + g * 0.534 + b * 0.131;
     }
     return pixelArray;
 }
