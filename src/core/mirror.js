5f23346a69711697790698bb8696a4803562838c
VinciGit00 <mvincig11@gmail.com>
2024-04-24 21:31:22 +0200

    add the 2 functions

--- src/core/mirror.js
+++ src/core/mirror.js
@@ -1,18 +1,17 @@
-import utils from "../utils.js";
-
 /**
  * Function to mirror an image horizontally
  * @param {number[]} pixelArray: Image pixel array in the format [R, G, B, alpha,..., R, G, B, alpha]
- * @param {number} width: Width of the image
- * @param {number} height: Height of the image
  * @returns {number[]} mirroredPixelArray: Mirrored image pixel array in the format [R, G, B, alpha,..., R, G, B, alpha]
  */
-function mirrorImageHorizontally(pixelArray, width, height) {
+function mirrorImageHorizontally(pixelArray) {
+    const pixelCount = pixelArray.length / 4; // Assuming each pixel has 4 values: R, G, B, alpha
+    const sideLength = Math.sqrt(pixelCount); // Assuming the image is square
+
     const mirroredPixelArray = [];
 
-    for (let y = 0; y < height; y++) {
-        for (let x = width - 1; x >= 0; x--) {
-            const pixelIndex = (y * width + (width - x - 1)) * 4;
+    for (let y = 0; y < sideLength; y++) {
+        for (let x = sideLength - 1; x >= 0; x--) {
+            const pixelIndex = (y * sideLength + (sideLength - x - 1)) * 4;
             mirroredPixelArray.push(pixelArray[pixelIndex]); // Red
             mirroredPixelArray.push(pixelArray[pixelIndex + 1]); // Green
             mirroredPixelArray.push(pixelArray[pixelIndex + 2]); // Blue
@@ -22,5 +21,3 @@ function mirrorImageHorizontally(pixelArray, width, height) {
 
     return mirroredPixelArray;
 }
-
-export default mirrorImageHorizontally;
---
+++ src/core/rotate_image.js
@@ -0,0 +1,69 @@
+import utils from "../utils.js";
+
+/**
+ * Function to rotate an image by a specified angle
+ * @param {number[]} pixelArray: Image pixel array in the format [R, G, B, alpha,..., R, G, B, alpha]
+ * @param {number} angle: Angle to rotate the image (in degrees)
+ * @returns {number[]} rotatedPixelArray: Rotated image pixel array in the format [R, G, B, alpha,..., R, G, B, alpha]
+ */
+function rotateImage(pixelArray, angle) {
+    const radians = angle * Math.PI / 180;
+    const cosTheta = Math.cos(radians);
+    const sinTheta = Math.sin(radians);
+
+    const width = Math.sqrt(pixelArray.length / 4);
+    const height = width;
+
+    const center_x = width / 2;
+    const center_y = height / 2;
+
+    const rotatedPixelArray = [];
+
+    for (let y = 0; y < height; y++) {
+        for (let x = 0; x < width; x++) {
+            const source_x = (x - center_x) * cosTheta - (y - center_y) * sinTheta + center_x;
+            const source_y = (x - center_x) * sinTheta + (y - center_y) * cosTheta + center_y;
+
+            if (source_x >= 0 && source_x < width && source_y >= 0 && source_y < height) {
+                const source_x1 = Math.floor(source_x);
+                const source_x2 = Math.ceil(source_x);
+                const source_y1 = Math.floor(source_y);
+                const source_y2 = Math.ceil(source_y);
+
+                const tX = source_x - source_x1;
+                const tY = source_y - source_y1;
+
+                const index1 = (source_y1 * width + source_x1) * 4;
+                const index2 = (source_y1 * width + source_x2) * 4;
+                const index3 = (source_y2 * width + source_x1) * 4;
+                const index4 = (source_y2 * width + source_x2) * 4;
+
+                const R1 = pixelArray[index1] * (1 - tX) + pixelArray[index2] * tX;
+                const R2 = pixelArray[index3] * (1 - tX) + pixelArray[index4] * tX;
+                const R = R1 * (1 - tY) + R2 * tY;
+
+                const G1 = pixelArray[index1 + 1] * (1 - tX) + pixelArray[index2 + 1] * tX;
+                const G2 = pixelArray[index3 + 1] * (1 - tX) + pixelArray[index4 + 1] * tX;
+                const G = G1 * (1 - tY) + G2 * tY;
+
+                const B1 = pixelArray[index1 + 2] * (1 - tX) + pixelArray[index2 + 2] * tX;
+                const B2 = pixelArray[index3 + 2] * (1 - tX) + pixelArray[index4 + 2] * tX;
+                const B = B1 * (1 - tY) + B2 * tY;
+
+                rotatedPixelArray.push(R);
+                rotatedPixelArray.push(G);
+                rotatedPixelArray.push(B);
+                rotatedPixelArray.push(pixelArray[index1 + 3]);
+            } else {
+                rotatedPixelArray.push(0); // Red
+                rotatedPixelArray.push(0); // Green
+                rotatedPixelArray.push(0); // Blue
+                rotatedPixelArray.push(0); // Alpha
+            }
+        }
+    }
+
+    return rotatedPixelArray;
+}
+
+export default rotateImage;
