bebf193b74968ca7715eb12a85e394f1d17c53a5
DPende <pendesini.daniele@gmail.com>
2024-05-02 17:05:41 +0200

    refactor: optimized image conversion from pixelArray to image

--- src/image-manager.js
+++ src/image-manager.js
@@ -1,92 +1,83 @@
-function getPixelArray(image) {
-    const { canvas, context } = createCanvas();
-    canvas.width = image.naturalWidth;
-    canvas.height = image.naturalHeight;
-    context.drawImage(image, 0, 0, image.naturalWidth, image.naturalHeight);
-    return context.getImageData(0, 0, image.naturalWidth, image.naturalHeight).data;
+const ImageManager = function () {
+    this.canvas = document.createElement("canvas")
+    this.context = this.canvas.getContext("2d", { willReadFrequently: true })
 }
 
-function resizeByPercentage(image, percentage) {
-    if (percentage < 1 || percentage > 100) 
-        throw new Error("The percentage value is incorrect: it must be between 1 and 100");
-    const { canvas, context } = createCanvas();
+ImageManager.prototype.getPixelArray = async function (image) {
+    const bitmap = await createImageBitmap(image);
+    this.canvas.width = bitmap.width;
+    this.canvas.height = bitmap.height;
+    this.context.drawImage(bitmap, 0, 0);
+    return this.context.getImageData(0, 0, bitmap.width, bitmap.height).data;
+}
+
+ImageManager.prototype.resizeByPercentage = async function (image, percentage) {
     const newWidth = image.naturalWidth * (percentage / 100);
     const newHeight = image.naturalHeight * (percentage / 100);
-    canvas.width = newWidth;
-    canvas.height = newHeight;
-    context.drawImage(image, 0, 0, newWidth, newHeight);
+    this.canvas.width = newWidth;
+    this.canvas.height = newHeight;
+    this.context.drawImage(image, 0, 0, newWidth, newHeight);
     return new Promise((resolve, reject) => {
-        let resizedImage = new Image();
-        resizedImage.onload = () => { resolve(resizedImage) }
-        resizedImage.onerror = (error) => { reject(error) }
-        resizedImage.src = canvas.toDataURL("image/" + getImageType(image.src));
+        this.canvas.toBlob((blob) => {
+            const image = new Image();
+            image.onload = () => { resolve(image) }
+            image.onerror = (error) => { reject(error) }
+            image.src = URL.createObjectURL(blob);
+        }, "image/" + this.getImageType(image.src));
     })
 }
 
-function resizeByWidth(image, newWidth) {
-    if(newWidth < 0) 
-        throw new Error("The width entered is invalid: it must be positive");
-    const { canvas, context } = createCanvas();
+ImageManager.prototype.resizeByWidth = async function (image, newWidth) {
     const newHeight = image.naturalHeight * (newWidth / image.naturalWidth);
-    canvas.width = newWidth;
-    canvas.height = newHeight;
-    context.drawImage(image, 0, 0, newWidth, newHeight);
+    this.canvas.width = newWidth;
+    this.canvas.height = newHeight;
+    this.context.drawImage(image, 0, 0, newWidth, newHeight);
     return new Promise((resolve, reject) => {
-        let resizedImage = new Image();
-        resizedImage.onload = () => { resolve(resizedImage) }
-        resizedImage.onerror = (error) => { reject(error) }
-        resizedImage.src = canvas.toDataURL("image/" + getImageType(image.src));
+        this.canvas.toBlob((blob) => {
+            const image = new Image();
+            image.onload = () => { resolve(image) }
+            image.onerror = (error) => { reject(error) }
+            image.src = URL.createObjectURL(blob);
+        }, "image/" + this.getImageType(image.src))
     })
 }
 
-function resizeByHeight(image, newHeight) {
-    if(newHeight < 0) 
-        throw new Error("the height entered is invalid: it must be positive");
-    const { canvas, context } = createCanvas();
+ImageManager.prototype.resizeByHeight = async function (image, newHeight) {
     const newWidth = image.naturalWidth * (newHeight / image.naturalHeight);
-    canvas.width = newWidth;
-    canvas.height = newHeight;
-    context.drawImage(image, 0, 0, newWidth, newHeight);
+    this.canvas.width = newWidth;
+    this.canvas.height = newHeight;
+    this.context.drawImage(image, 0, 0, newWidth, newHeight);
     return new Promise((resolve, reject) => {
-        let resizedImage = new Image();
-        resizedImage.onload = () => { resolve(resizedImage) }
-        resizedImage.onerror = (error) => { reject(error) }
-        resizedImage.src = canvas.toDataURL("image/" + getImageType(image.src));
+        this.canvas.toBlob((blob) => {
+            const image = new Image();
+            image.onload = () => { resolve(image) }
+            image.onerror = (error) => { reject(error) }
+            image.src = URL.createObjectURL(blob);
+        }, "image/" + this.getImageType(image.src))
     })
 }
 
-function convertToImage(pixelArray, width, height, type) {
-    const { canvas, context } = createCanvas();
-    canvas.width = width;
-    canvas.height = height;
-    let imageData = context.createImageData(width, height);
-    imageData.data.set(new Uint8ClampedArray(pixelArray));
-    context.putImageData(imageData, 0, 0);
+ImageManager.prototype.convertToImage = async function (pixelArray, width, height, type) {
+    this.canvas.width = width;
+    this.canvas.height = height;
+    const imageData = this.context.createImageData(width, height);
+    imageData.data.set(pixelArray);
+    this.context.putImageData(imageData, 0, 0);
     return new Promise((resolve, reject) => {
-        let image = new Image();
-        image.onload = () => { resolve(image) }
-        image.onerror = (error) => { reject(error) }
-        image.src = canvas.toDataURL("image/" + type);
+        this.canvas.toBlob((blob) => {
+            const image = new Image();
+            image.onload = () => { resolve(image) }
+            image.onerror = (error) => { reject(error) }
+            image.src = URL.createObjectURL(blob);
+        }, "image/" + type)
     })
 }
 
-function getImageType(imageSrc){
-    const regex = /(jpg|jpeg|png|gif|bmp|webp)/;
-    return imageSrc.match(regex)[0];
+ImageManager.prototype.getImageType = function (imageSrc) {
+    const result = imageSrc.match(/(jpg|jpeg|png|gif|bmp|webp)/)[0]
+    if (result === "jpg")
+        return "jpeg"
+    return result;
 }
 
-function createCanvas() {
-    const canvas = document.createElement("canvas")
-    const context = canvas.getContext("2d")
-    context.imageSmoothingEnabled = true;
-    return { canvas: canvas, context: context };
-}
-
-export default {
-    getPixelArray,
-    resizeByPercentage,
-    resizeByWidth,
-    resizeByHeight,
-    convertToImage,
-    getImageType
-}
+export default ImageManager;
